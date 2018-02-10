6b3ad8ee58ad487360530ce2b66ebfcdc34f1fe2
Idaho Frost <frostidaho@gmail.com>
2018-02-10 14:25:36 -0500

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,12 @@
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
 
 __version__ = '0.0.2'
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.org'), encoding='utf-8') as f:
-    long_description = f.read()
-
-# get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
-
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
 
 setup(
     name='dbusnotify',
     version=__version__,
     description='dbusnotify is a library for creating desktop notifications using the freedesktop.org notification spec',
-    long_description=long_description,
+    long_description='dbusnotify is a library for creating desktop notifications using the freedesktop.org notification spec',
     url='https://github.com/frostidaho/python-dbusnotify',
     download_url='https://github.com/frostidaho/python-dbusnotify/tarball/' + __version__,
     license='BSD',
@@ -34,7 +19,5 @@ setup(
     packages=find_packages(exclude=['docs', 'tests*', 'examples']),
     include_package_data=True,
     author='Frost Idaho',
-    install_requires=install_requires,
-    dependency_links=dependency_links,
     author_email='frostidaho@gmail.com'
 )
